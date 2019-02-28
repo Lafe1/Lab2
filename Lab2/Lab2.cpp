692506dceb0e8f724b0fbf3f1d92a2a89001cd6e
Lafe1 <43574247+Lafe1@users.noreply.github.com>
2019-02-28 21:42:29 +0300

    Добавлены арифметические действия

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -18,14 +18,50 @@ private:
 	//int n; //кол-во координат в векторе
 	//double *real = new double[n],
 	//	*imaginary = new double[n];
-	int real,   //Вещественная часть
+	double 
+		real,   //Вещественная часть
 		imaginary;   //мнимая
 public:
 	//конструкторы
 	Complex();
 	Complex(double r, double i);
 	void show(int i);
+	void show();
+	//деструктор
+	~Complex() {};
+	//перегрузки
+	Complex operator+(Complex b);
+	Complex operator-(Complex b);
+	Complex operator*(Complex b);
+	Complex operator/(Complex b);
+	Complex operator=(Complex b);
 };
+
+Complex Complex::operator+(Complex b)
+{
+	return Complex(this->real + b.real, this->imaginary + b.imaginary);
+}
+
+Complex Complex::operator-(Complex b)
+{
+	return Complex(this->real - b.real, this->imaginary - b.imaginary);
+}
+
+Complex Complex::operator*(Complex b)
+{
+	return Complex((this->real * b.real) - (this->imaginary * b.imaginary), (this->imaginary * b.real) + (this->real * b.imaginary));
+}
+
+Complex Complex::operator/(Complex b)
+{
+	return Complex(((this->real * b.real) + (this->imaginary * b.imaginary))/((b.real * b.real) + (b.imaginary*b.imaginary)), ((this->imaginary * b.real) - (this->real * b.imaginary)) / ((b.real * b.real) + (b.imaginary * b.imaginary)));
+}
+
+Complex Complex::operator=(Complex b)
+{
+	return Complex(this->real = b.real, this->imaginary = b.imaginary);
+}
+
 
 Complex::Complex()
 {
@@ -33,9 +69,20 @@ Complex::Complex()
 	imaginary = rand() % 10 - 5;
 }
 
+Complex::Complex(double r, double i)
+{
+	real = r;
+	imaginary = i;
+}
+
 void Complex::show(int i)
 {
-	cout << "Комплексное число "<<i<<": " << real << " + (" << imaginary << "i)" << endl;
+	cout << "  Комплексное число "<<i<<": " << real << " + (" << imaginary << "i)" << endl;
+}
+
+void Complex::show()
+{
+	cout << real << " + (" << imaginary << "i)" << endl;
 }
 
 //******************** MAIN **************************** 
@@ -45,13 +92,33 @@ void main()
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251); 
 	SetConsoleOutputCP(1251);
-	int n = 10;
-	Complex *comp;
-	comp = new Complex[n];
-	for (int i = 0; i < n; i++)
-	{
-		comp[i].show(i);
-	}
+	double n = 2,
+		real,
+		imaginary;
+	//Complex *comp0,
+	//		*comp1;
+	cout << "**************************** COMPLEX ****************************\n"
+		<< "=======================================================================\n"
+		<< "||  Данная программа выполняет арифмитические действия с              ||\n"
+		<< "|| комплексными числами и сложение комплексных векторов.              ||\n"
+		<< "=======================================================================\n\n"
+		<< "  Ввод исходных данных\n"
+		<< "======================\n";
+	cout << " Целая часть первого комплексного числа: "; cin >> real;
+	cout << " Мнимая часть первого комплексного числа: "; cin >> imaginary;
+	Complex comp0 = Complex(real, imaginary);
+	comp0.show(1);
+	cout << " Целая часть второго комплексного числа: "; cin >> real;
+	cout << " Мнимая часть второго комплексного числа: "; cin >> imaginary;
+	Complex comp1 = Complex(real, imaginary);
+	comp1.show(2);
+	cout << "  Арифметические действия с комплексными числами\n "
+		 << "======================\n";
+	cout << " 1. Сумма: "; (comp0 + comp1).show();
+	cout << " 2. Разность: "; (comp0 - comp1).show();
+	cout << " 3. Произведение: "; (comp0 * comp1).show();
+	cout << " 4. Частное: "; (comp0 / comp1).show();
+	cout << " 5. Присваивание: "; (comp0 = comp1).show();
 
 	while (!_kbhit());
 }
