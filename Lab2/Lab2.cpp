5aced92db5999c06b0a142e4a647690e9900d5e2
Lafe1 <43574247+Lafe1@users.noreply.github.com>
2019-03-01 21:09:35 +0300

    Добавлены вектора с комлексными координатами. Программа заверщена.

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -18,7 +18,7 @@ private:
 	//int n; //кол-во координат в векторе
 	//double *real = new double[n],
 	//	*imaginary = new double[n];
-	double 
+	double
 		real,   //Вещественная часть
 		imaginary;   //мнимая
 public:
@@ -54,7 +54,7 @@ Complex Complex::operator*(Complex b)
 
 Complex Complex::operator/(Complex b)
 {
-	return Complex(((this->real * b.real) + (this->imaginary * b.imaginary))/((b.real * b.real) + (b.imaginary*b.imaginary)), ((this->imaginary * b.real) - (this->real * b.imaginary)) / ((b.real * b.real) + (b.imaginary * b.imaginary)));
+	return Complex(((this->real * b.real) + (this->imaginary * b.imaginary)) / ((b.real * b.real) + (b.imaginary*b.imaginary)), ((this->imaginary * b.real) - (this->real * b.imaginary)) / ((b.real * b.real) + (b.imaginary * b.imaginary)));
 }
 
 Complex Complex::operator=(Complex b)
@@ -77,12 +77,29 @@ Complex::Complex(double r, double i)
 
 void Complex::show(int i)
 {
-	cout << "  Комплексное число "<<i<<": " << real << " + (" << imaginary << "i)" << endl;
+	cout << "  Комплексное число " << i << ": " << real << " + (" << imaginary << "i)" << endl;
 }
 
 void Complex::show()
 {
-	cout << real << " + (" << imaginary << "i)" << endl;
+	cout << real << " + (" << imaginary << "i)";
+}
+
+void vect_sum(Complex comp3[], Complex comp4[], int n)
+{
+	Complex *comp5 = new Complex[n];
+	for (int i = 0; i < n; i++)
+	{
+		comp5[i] = comp3[i] + comp4[i];
+	}
+	cout << " Сумма векторов:" << endl;
+	cout << " Vector3{";
+	for (int j = 0; j < n; j++)
+	{
+		comp5[j].show();
+		if (j != (n - 1))cout << ", ";
+	}
+	cout << "}.\n";
 }
 
 //******************** MAIN **************************** 
@@ -90,35 +107,84 @@ void main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "Russian");
-	SetConsoleCP(1251); 
+	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	double n = 2,
-		real,
+	int n;
+	double real,
 		imaginary;
-	//Complex *comp0,
-	//		*comp1;
 	cout << "**************************** COMPLEX ****************************\n"
 		<< "=======================================================================\n"
 		<< "||  Данная программа выполняет арифмитические действия с              ||\n"
 		<< "|| комплексными числами и сложение комплексных векторов.              ||\n"
 		<< "=======================================================================\n\n"
-		<< "  Ввод исходных данных\n"
-		<< "======================\n";
+		<< "  1. Ввод исходных данных\n"
+		<< "=========================\n\n";
 	cout << " Целая часть первого комплексного числа: "; cin >> real;
 	cout << " Мнимая часть первого комплексного числа: "; cin >> imaginary;
 	Complex comp0 = Complex(real, imaginary);
 	comp0.show(1);
+	cout << endl;
 	cout << " Целая часть второго комплексного числа: "; cin >> real;
 	cout << " Мнимая часть второго комплексного числа: "; cin >> imaginary;
 	Complex comp1 = Complex(real, imaginary);
 	comp1.show(2);
-	cout << "  Арифметические действия с комплексными числами\n "
-		 << "======================\n";
+	cout << endl;
+	cout << " Арифметические действия с комплексными числами\n "
+		<< "==============================================\n\n";
 	cout << " 1. Сумма: "; (comp0 + comp1).show();
+	cout << endl;
 	cout << " 2. Разность: "; (comp0 - comp1).show();
+	cout << endl;
 	cout << " 3. Произведение: "; (comp0 * comp1).show();
+	cout << endl;
 	cout << " 4. Частное: "; (comp0 / comp1).show();
+	cout << endl;
 	cout << " 5. Присваивание: "; (comp0 = comp1).show();
+	cout << endl << endl;
+	cout << "  2. Ввод исходных данных\n"
+		<< "=========================\n\n"
+		<< "  Размерность вектора: "; cin >> n;
+	cout << endl;
+	Complex *comp3 = new Complex[n],
+		*comp4 = new Complex[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "  Координата первого вектора №" << i << endl
+			<< "================================\n";
+		cout << " Целая часть: "; cin >> real;
+		cout << " Мнимая часть: "; cin >> imaginary;
+		comp3[i] = Complex(real, imaginary);
+		cout << endl;
+	}
+	cout << endl;
+	for (int j = 0; j < n; j++)
+	{
+		cout << "  Координата второго вектора №" << j << endl
+			<< "================================\n";
+		cout << " Целая часть: "; cin >> real;
+		cout << " Мнимая часть: "; cin >> imaginary;
+		comp4[j] = Complex(real, imaginary);
+		cout << endl;
+	}
+	cout << endl << " Вектора\n "
+		<< "========\n\n";
+	cout << " Первый вектор:" << endl;
+	cout << " Vector1{";
+	for (int i = 0; i < n; i++)
+	{
+		comp3[i].show();
+		if (i != (n - 1))cout << ", ";
+	}
+	cout << "}.\n\n";
+	cout << " Второй вектор:" << endl;
+	cout << " Vector2{";
+	for (int j = 0; j < n; j++)
+	{
+		comp4[j].show();
+		if (j != (n - 1))cout << ", ";
+	}
+	cout << "}.\n\n";
+	vect_sum( comp3, comp4, n);
 
 	while (!_kbhit());
 }
